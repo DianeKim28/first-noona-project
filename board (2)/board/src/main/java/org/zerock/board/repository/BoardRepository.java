7f098b8f8be31a7c1c0e3615938794e684282a4c
DianeKim28 <k76390915@gmail.com>
2023-04-18 19:08:56 +0900

    first commit

---
+++ board (2)/board/src/main/java/org/zerock/board/repository/BoardRepository.java
@@ -0,0 +1,10 @@
+package org.zerock.board.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.zerock.board.entity.Board;
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    @Query("select n, w from Board b left join b. writer w where b. bno =:bno")
+    Object getBoardWithWriter(@Param("bno")Long bno);
+}
